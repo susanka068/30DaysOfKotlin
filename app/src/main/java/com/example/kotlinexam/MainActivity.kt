1e30bb06c0593944b603e1a565536fadeded92c0
susanka068 <sathinandimajumder068@gmail.com>
2020-06-05 18:02:46 +0530

    ..

--- app/src/main/java/com/example/kotlinexam/MainActivity.kt
+++ app/src/main/java/com/example/kotlinexam/MainActivity.kt
@@ -11,8 +11,8 @@ import android.widget.Toast
 import android.widget.TextView
 import android.widget.ImageView
 
-val quoteList = listOf(Pair("Even your greatest mistakes shouldn't stop you from moving forward." , "Edward Elric"),Pair("I'll take a potato chip....and eat it!","Light Yagami"),Pair("the circumstances of one's birth are irrevelant. It is what you do with the gift of life that determines who you are.","Mewtwo"),Pair("It is never a crime to just exist, no matter how dangerous your weapons are.","Franky"))
-var iterInt = 0 ;
+val quoteList = listOf(Pair("Even your greatest mistakes shouldn't stop you from moving forward." , "Edward Elric"),Pair("I'll take a potato chip....and eat it!","Light Yagami"),Pair("the circumstances of one's birth are irrevelant. It is what you do with the gift of life that determines who you are.","Mewtwo"),Pair("It is never a crime to just exist, no matter how dangerous your weapons are.","Franky"),Pair("There are times when working hard may not be enough. But those who succeed worked hard.","Coach Kamogawa"),Pair("I have two rules: first, I'm never wrong. Second, if I'm wrong, back to the first rule","L"),Pair("Life isn't about what river you live in but how you swim in the river you live in.","Koro Sensei"),Pair("I've lost my pawn, my knight, my rook, my bishop and even my queen. But it's not checkmate just yet.","Roy Mustang"),Pair("I'm glad I said sorry","Ichimaru Gin"))
+var iterInt = -1 ;
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
